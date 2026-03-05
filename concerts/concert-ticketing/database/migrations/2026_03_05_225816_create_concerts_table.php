acdc716e33c313ad4129da50d9262cce43f33043
MonsieurTeal <nicu.dan25@gmail.com>
2026-03-06 01:35:26 +0200

    migrations to db

---
+++ concerts/concert-ticketing/database/migrations/2026_03_05_225816_create_concerts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('concerts', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignId('band_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('date');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('concerts');
+    }
+};
